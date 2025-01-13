8eb233a46559b0b3f825cf8546d4eb4837b2ff07
G Rosset <138031874+gustavorosset1@users.noreply.github.com>
2025-01-13 11:24:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 // //alert("Hello World");
 
+//EXERCIOCIO STRINGS
+
 const nome = prompt('Digite seu nome')
 
 document.body.innerHTML += `Seu nome é: <strong>${nome}</strong><br />`;
@@ -15,3 +17,19 @@ document.body.innerHTML += `As últimas 3 letras do seu nome são: <strong>${nom
 document.body.innerHTML += `As letras do seu nome são: <strong>${nome.split('')} </strong><br />`
 document.body.innerHTML += `Seu nome com letras maiúsculas: <strong>${nome.toUpperCase()} </strong><br />`
 document.body.innerHTML += `Seu nome com letras minúsculas: <strong>${nome.toLowerCase()} </strong><br />`
+
+// EXERCICIO MATH 
+
+const numero = Number(prompt('Digite um número:'));
+const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
+
+numeroTitulo.innerHTML = numero;
+
+texto.innerHTML = ''; 
+texto.innerHTML += `<p>Raiz quadrada: ${numero ** 0.5}.</p>`;
+texto.innerHTML += `<p>É NaN: ${Number.isNaN(numero)}.</p>`;
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}.</p>`;
+texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}.</p>`;
+texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}.</p>`;
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}.</p>`; 
